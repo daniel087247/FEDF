2dcf4f7e607ed58a6c43ae992988bd47d7440d9a
daniel087247 <2420080087@klh.edu.in>
2025-10-22 10:25:28 +0530

    file created

---
+++ Week_7/data.js
@@ -0,0 +1,7 @@
+export const books = [ 
+{ title: "JavaScript Essentials", author: "John Doe", price: 499, availability: "in stock" 
+}, 
+{ title: "DeepSeek", author: "Jane Smith", price: 890, availability: "in stock" }, 
+{ title: "Deep Learning with Java", author: "Mark Lee", price: 987, availability: "out of stock" }, 
+{ title: "Learn with C", author: "Ramya Krishna", price: 538, availability: "out of stock" } 
+];
